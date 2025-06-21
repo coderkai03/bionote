4539ef928404f398fcc440c99f9e3362e5be905c
Priyansh Shah <Priyanshpokemon@gmail.com>
2025-06-21 16:51:04 -0700

    adding image-gen hopefully

--- app/api/image-generation/route.ts
+++ app/api/image-generation/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
