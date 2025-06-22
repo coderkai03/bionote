bf0048ab39f20fe0caf61d3e58ab28ab01da4fab
Priyansh Shah <Priyanshpokemon@gmail.com>
2025-06-22 05:29:52 -0700

    fix: adding constants and and naming them respectively

---
+++ app/lib/RespectiveConstants.ts
@@ -0,0 +1,20 @@
+export const MeshyModels = {
+  "Steam Train": "/public/Steam_Engine.glb",
+  "Lock and Key": "/public/Lock_and_Key.glb",
+  "Car V8 Engine": "/public/V8_Car_Engine.glb",
+  "Crane": "/public/Crane.glb",
+  "Camera Insides": "/public/Camera_Insides.glb",
+};
+
+export const SketchFabModels = {
+  pancreas:
+    "https://sketchfab.com/3d-models/pancreas-58cf045dce9a45e9b08235bc26209aa3",
+  ear: "https://sketchfab.com/3d-models/ear-cross-section-4f5438fc9337454587ec4a2c30c8c42f",
+  bird: "https://sketchfab.com/3d-models/bird-internal-organs-9e80184bfdb1472b88077fca9fa24217",
+  skeleton:
+    "https://sketchfab.com/3d-models/the-human-spinal-column-bcd9eee09ce044ef98a69c315aa792e2",
+  kidney:
+    "https://sketchfab.com/3d-models/kidney-3aef2741ea754fb486451292b87e159a",
+  brain:
+    "https://sketchfab.com/3d-models/human-brain-e073c2590bc24daaa7323f4daa5b7784",
+};
